a3fe56c89626d01c93ce786d047bdcddba6ce09b
TheFluffyNebula <jasonyin30@gmail.com>
2026-03-27 17:06:55 -0400

    config: disabling image optimization so they show up also, frontend/public folder restructuring

--- frontend/src/static/about.js
+++ frontend/src/static/about.js
@@ -2,21 +2,21 @@ let about = [
     {
         id: 1,
         title: "Wind Energy",
-        img_path: "/wind.jpg",
+        img_path: "/wind/wind.jpg",
         info: "Wind power is the use of wind energy to generate useful work. Historically, wind power was used by sails, windmills and windpumps, but today it is mostly used to generate electricity.",
         redirect: "/about/wind"
     },
     {
         id: 2,
         title: "Wave Energy",
-        img_path: "/wave.jpg",
+        img_path: "/wave/wave.jpg",
         info: "Wave power is the capture of energy of wind waves to do useful work – for example, electricity generation, water desalination, or pumping water. A machine that exploits wave power is a wave energy converter.",
         redirect: "/about/wave"
     },
     {
         id: 3,
         title: "Ocean Energy (Kites)",
-        img_path: "/kites.jpg",
+        img_path: "/kites/kites.jpg",
         info: "Underwater kites, also known as tidal kites or sea dragons, generate electricity from ocean currents by converting kinetic energy to electricity",
         redirect: "/about/kites"
     },
